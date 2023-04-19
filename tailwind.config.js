6a6898d546f600924fde5dc62286d49fa10a4611
samervalente <samervalente@gmail.com>
2023-04-19 16:21:16 -0300

    config: add initial tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        'blue': {
+          'main':'#00D1FF',
+          700:'#34A5BE'
+        },
+        'gray': {
+          300:'#E0E0E0',
+          400:'#BBBBBB',
+          500:'#666666',
+          600:'#444444',
+          700:'#2A2A2A'
+        }
+      }
     },
   },
   plugins: [],
