c1dc99cfec02db3b74e65f4d80945555b634855e
samervalente <samervalente@gmail.com>
2023-05-05 19:06:55 -0300

    create signup schema

--- src/schemas/signup.ts
+++ src/schemas/signup.ts
@@ -1,12 +1,12 @@
 import * as yup from 'yup'
 
 export const signupSchema = yup.object({
-    firstname: yup.string().min(3, "Mínimo 3 caracteres").required("Campo obrigatório"),
-    surname: yup.string().required("Campo obrigatório"),
-    gender: yup.string().required("Campo obrigatório"),
-    birthDate: yup.string().required("Campo obrigatório"),
-    email: yup.string().email("Insira um email válido").required("Campo obrigatório"),
-    phone: yup.string().matches(/^(\([1-9]{2}\)|[1-9]{2})\s?([9]{1})?\s?[0-9]{4}\-?[0-9]{4}$/, "Telefone inválido").required("Campo obrigatório"),
-    password: yup.string().min(8, "Mínimo 8 caracteres").required("Campo obrigatório"),
-    passwordConfirmation: yup.string().required("Campo obrigatório").oneOf([yup.ref('password')], "Senhas não conferem.")
+    firstname: yup.string().min(3, "Mínimo 3 caracteres").required("Nome é obrigatório"),
+    surname: yup.string().required("Sobrenome é obrigatório"),
+    gender: yup.string().required("Gênero é obrigatório"),
+    birthDate: yup.string().required("Data de nascimento é obrigatória"),
+    email: yup.string().email("Insira um email válido").required("Email é obrigatório"),
+    phone: yup.string().matches(/^(\([1-9]{2}\)|[1-9]{2})\s?([9]{1})?\s?[0-9]{4}\-?[0-9]{4}$/, "Telefone inválido").required("Telefone é obrigatório"),
+    password: yup.string().min(8, "Mínimo 8 caracteres").required("Senha é obrigatória"),
+    passwordConfirmation: yup.string().oneOf([yup.ref('password')], "Senhas não conferem.")
 })
